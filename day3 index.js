0a74500408238fe4ac36ef855e54781987016ea5
adityasrivastava19 <adityasrivastava2317@gmail.com>
2026-01-18 09:25:22 +0530

    Add CLI for counting words and lines
    Implemented a CLI tool to count words and lines in a file using Commander.js.

---
+++ day3 index.js
@@ -0,0 +1,45 @@
+const { Command } = require('commander');
+const program = new Command();
+const fs=require('fs');
+
+program
+  .name('word')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
+
+program.command('word')
+  .description('count the word inside the file ')
+  .argument('<file>', 'count the words')
+  .action((file) => {
+   fs.readFile(file,"utf-8",function(err,data)
+  {
+    let total =0;
+    for(let i=0;i<data.length;i++)
+    {
+      if(data[i]==" ")
+      {
+        total++;
+      }
+    }
+    console.log(total+1);
+  })
+  });
+
+  program.command('line')
+  .description('count the line in the file ')
+  .argument('<file>', 'count the line')
+  .action((file) => {
+   fs.readFile(file,"utf-8",function(err,data)
+  {
+    let total =0;
+    for(let i=0;i<data.length;i++)
+    {
+      if(data[i]=="\n")
+      {
+        total++;
+      }
+    }
+    console.log(total+1);
+  })
+  });
+program.parse();
